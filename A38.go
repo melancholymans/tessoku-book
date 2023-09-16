749ae23a00ed226b73686da3b58f980bc02c9ab6
joshua <mattbew64@gmail.com>
2023-09-16 11:09:43 +0900

    A38.go add

---
+++ A38.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func main() {
+	defer wtr.Flush()
+	d, n := ni2()
+	sl := make([]int, d)
+	for i := 0; i < d; i++ {
+		sl[i] = 24
+	}
+	for i := 0; i < n; i++ {
+		l := ni() - 1
+		r := ni() - 1
+		h := ni()
+		for j := l; j <= r; j++ {
+			sl[j] = Min(sl[j], h)
+		}
+	}
+	total := 0
+	for i := 0; i < d; i++ {
+		total += sl[i]
+	}
+	fmt.Fprintln(wtr, total)
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
